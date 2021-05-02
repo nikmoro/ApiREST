761c12e76371e2a67fa342b06ed75329f8158506
Osiel Nicolás <chilparosales@gmail.com>
2021-05-01 23:53:34 -0500

    Agregar archivos de proyecto.

---
+++ ApiRest/Controllers/EjemploController.cs
@@ -0,0 +1,48 @@
+﻿using ApiRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiRest.Controllers
+{
+    public class EjemploController : ApiController
+    {
+
+        [HttpGet]
+        public List<Donacion> GetLista()
+        {
+            using (var conexion = new ReforestacionEntities())
+            {
+                var result = (from re in conexion.Donaciones select re);
+                return result.ToList();
+            }
+        }
+
+        [HttpPost]
+        public bool Insertar(Donacion entidad)
+        {
+            using (var conexion = new ReforestacionEntities())
+            {
+                try
+                {
+                    conexion.Donaciones.Add(new Donacion()
+                    {
+                        nombre_mpio = entidad.nombre_mpio,
+                        nombre_arbol = entidad.nombre_arbol,
+                        cantidad = entidad.cantidad
+                    });
+                    conexion.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                
+            }
+        }
+    }
+}
