7699e9371b000155abf12eacec82139baa613388
Osiel Nicolás <chilparosales@gmail.com>
2021-05-06 15:58:13 -0500

    Se separaron los datos de modelo y la instancia del modelo en la clase parcial Index.

---
+++ BlazorServerApiRestClient/Data/Donacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApiRestClient.Data
+{
+    public class Donacion
+    {
+        public int id { get; set; } = 0;
+        public string nombre_mpio { get; set; } = "";
+        public string nombre_arbol { get; set; } = "";
+        public int cantidad { get; set; } = 0;
+    }
+}
---
+++ BlazorServerApiRestClient/Pages/Index.cs
@@ -0,0 +1,19 @@
+﻿using BlazorServerApiRestClient.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApiRestClient.Pages
+{
+    public partial class Index
+    {
+        List<Donacion> donaciones = new List<Donacion>();
+
+        private async Task GetDonaciones()
+        {
+            donaciones = await Http.GetJsonAsync<List<Donacion>>("http://www.apidonaciones.somee.com/api/Donacion");
+        }
+    }
+}
